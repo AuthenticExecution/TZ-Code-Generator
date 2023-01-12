33dfa22cc026e576de59ab0b007ecd50551ddd60
Gianluca Scopelliti <gianluca.scopelliti@ericsson.com>
2023-01-12 04:18:18 -0800

    increase stack and heap size

--- tzcodegen/stubs/user_ta_header_defines.h
+++ tzcodegen/stubs/user_ta_header_defines.h
@@ -10,9 +10,9 @@
 #define TA_FLAGS			TA_FLAG_EXEC_DDR
 
 /* Provisioned stack size */
-#define TA_STACK_SIZE			(2 * 1024)
+#define TA_STACK_SIZE			(1024 * 1024)
 
 /* Provisioned heap size for TEE_Malloc() and friends */
-#define TA_DATA_SIZE			(32 * 1024)
+#define TA_DATA_SIZE			(4 * 1024 * 1024)
 
 #endif
